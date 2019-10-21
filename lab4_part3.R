20539ee5d00fcd4c927f971e0cb2a5decf69c218
dglave <glave@ucsb.edu>
2019-10-21 13:57:32 -0700

    made graph of infant mortality rate

---
+++ lab4_part3.R
@@ -0,0 +1,23 @@
+#-------------
+# Dylan
+#------------
+
+library(tidyverse)
+library(janitor)
+library(here)
+
+# Read in Data: 
+
+db <- read_csv(here::here("data", "disease_burden.csv")) %>% 
+  clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+# Create a subset, then graph:
+db_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+# Plot
+
+ggplot(data = db_sub, aes( x = year, y = deaths_per_100k)) +
+  geom_line(aes(color = country_name))
